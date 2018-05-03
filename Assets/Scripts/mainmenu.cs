c864ca57ec8485d26243b323e41359765c35d18c
Saraporn Viriyarojanakul <senaleron@gmail.com>
2018-05-04 03:35:23 +0700

    add start menu

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour {
+
+    // Use this for initialization
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneLoader(1);
+        }
+    }
+}
