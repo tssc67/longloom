da684ee52da67239e08d93c62abd9ace82ae275c
north1432 <srv13_27@hotmail.com>
2018-05-03 20:35:52 +0700

    stage from pluem, modified wire, wirecontroller (not finished)

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameLogic
+{
+    public static Wire[,] wireBlock = new Wire[256, 256];
+    // public static GameObject[,] Alien = new GameObject[256, 256];
+    public static List<GameObject> Obj = new List<GameObject>();
+    
+
+    public static void Update()
+    {
+
+    }
+
+
+}
--- Assets/Scripts/WiredController.cs
+++ Assets/Scripts/WiredController.cs
@@ -2,72 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WiredController : MonoBehaviour {
+public class WiredController : MonoBehaviour
+{
+    
+    public List<Pair> markedWire;
 
-	//public List<GameObject> objs;
-	//public List<Pair> markedWire;
+    // call this everytime new structure or wire is placed
+    public void OnPlace()
+    {
+        // remove old connected status
+        foreach (GameObject obj in GameLogic.Obj)
+        {
+            if (obj.GetComponent<Structure>() != null)
+            {
+                obj.GetComponent<Structure>().ResetConnect();
+            }
+            else if (obj.GetComponent<Wire>() != null)
+            {
+                obj.GetComponent<Wire>().ResetWireGroup();
+            }
+        }
+        // markedWire.Clear();
 
-	//public void onPlace()
-	//{
-	//	foreach (GameObject obj in objs)
-		
-	//		if (obj.GetComponent<MonoBehaviour>() is Structure)
-	//		{
-	//			obj.GetComponent<Structure>().ResetConnect ();
-	//		}
-	//	}
-	//	int setNumber = 0;
-	//	foreach (GameObject obj in objs)
-	//	{
-	//		if (obj is Wire && markedWire.Any(p => p.first != obj))
-	//		{
-	//			markedWire.Add(Pair(obj, setNumber));
-	//			recursiveCheck(obj, setNumber);
-	//			setNumber++;
-	//		}
-	//	}
+        // group every wire together
+        int setNumber = 0;
+		foreach (GameObject obj in GameLogic.Obj)
+		{
+            var wireObj = obj.GetComponent<Wire>();
+            // if it's a wire
+			if (wireObj != null)
+			{
+                if (wireObj.wireGroup != -1)
+                {
+                    wireObj.SetWireGroup(setNumber);
+                    recursiveCheck(wireObj, setNumber);
+                    setNumber++;
+                }
+				// markedWire.Add(new Pair(obj, setNumber));
+			}
+		}
 
-	//	List<List<GameObject>> structure;
-	//	foreach (GameObject obj in Object)
-	//	{
-	//		if (obj is Structure)
-	//		{
-	//			GameObject x = GameLogic.Wire.getObjectAt(obj.x-1, obj.y);
-	//			if (x != null)
-	//			{
-	//				structure[markedWire.Where(p => p.Key == x)].Add(obj);
+		List<GameObject>[] structure = new List<GameObject>[setNumber];
+		foreach (GameObject obj in objs)
+		{
+            var structureObj = obj.GetComponent<Structure>();
+			if (structureObj != null)
+			{
+				GameObject x = GameLogic.Wire.getObjectAt(obj.x - 1, obj.y);
+				if (x != null)
+				{
+					structure[markedWire.Where(p => p.Key == x)].Add(obj);
 
-	//			}
-	//		}
-	//	}
-	//	for (int i = 0; i<setNumber; i++)
-	//	{
-	//		foreach (GameObject obj in Structure[i])
-	//		{
-	//			foreach (GameObject obj2 in Structure[i])
-	//			{
-	//				if (obj != obj2)
-	//				{
-	//					obj.connected(obj2)
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
+				}
+			}
+		}
+		for (int i = 0; i<setNumber; i++)
+		{
+			foreach (GameObject obj in Structure[i])
+			{
+				foreach (GameObject obj2 in Structure[i])
+				{
+					if (obj != obj2)
+					{
+						obj.connected(obj2)
+					}
+				}
+			}
+		}
+	}
 
-	//private void recursiveCheck(GameObject obj, int setNumber)
-	//{
-	//	GameObject x = GameLogic.Wire.getObjectAt(obj.x-1, obj.y);
-	//	if (x != null)
-	//	{
-	//		markedWire.Add(new Pair(x, setNumber));
-	//		recursiveCheck(x, setNumber);
-	//	}
-	//	x = GameLogic.Wire.getObjectAt(obj.x+1, obj.y);
-	//	if (x != null)
-	//	{
-	//		markedWire.Add(new Pair(x, setNumber));
-	//		recursiveCheck(x, setNumber);
-	//	}
-	//}
+	private void recursiveCheck(Wire obj, int setNumber)
+    {
+        int x = MapController.GetBlockX(obj.transform.position);
+        int y = MapController.GetBlockY(obj.transform.position);
+        Wire w = GameLogic.wireBlock.[y - 1, x];
+
+        if (w != null) // a wire object
+        {
+            if (w.wireGroup == -1) // does not have group
+            {
+                w.SetWireGroup(setNumber);
+                recursiveCheck()
+            }
+            markedWire.Add(new Pair(x, setNumber));
+            recursiveCheck(x, setNumber);
+        }
+        x = GameLogic.Wire.getObjectAt(obj.x + 1, obj.y);
+        if (x != null)
+        {
+            markedWire.Add(new Pair(x, setNumber));
+            recursiveCheck(x, setNumber);
+        }
+    }
 }
